8b57350ff9e88de6c951a6f415f961c42d146b06
sgtDentons <sgt.dentons@gmail.com>
2017-03-29 21:57:37 +0200

    Add POI related Activities

---
+++ app/src/main/java/pl/lodz/p/pathfinder/PoiFragmentPagerAdapter.java
@@ -0,0 +1,91 @@
+package pl.lodz.p.pathfinder;
+
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.lodz.p.pathfinder.model.PointOfInterest;
+
+
+/**
+ * Created by QDL on 2017-01-17.
+ */
+
+public class PoiFragmentPagerAdapter extends FragmentPagerAdapter
+{
+
+    private final static int PAGE_COUNT = 4;
+//    private String[] tabTitles = {"my trips","Browse"}; //FIXME
+    private String[] tabTitles;
+    private Context context;
+
+    public PoiFragmentPagerAdapter(FragmentManager fm, Context context)
+    {
+        super(fm);
+        this.context = context;
+        this.tabTitles = context.getResources().getStringArray(R.array.poi_menu_tabs);
+    }
+
+
+    @Override
+    public int getCount()
+    {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        return tabTitles[position];
+    }
+
+    //FIXME
+    @Override
+    public Fragment getItem(int position) {
+        Fragment f= null;
+        switch (position){
+            case 0:
+
+                //FIXME
+                f =  PoiListFragmentOpenEdit.newInstance(testData());
+            break;
+            case 1:
+                f =  PoiMenuTabSharedFragment.newInstance("bvdxz","F");
+            break;
+            case 2:
+                f =  PoiMenuTabSharedFragment.newInstance("bvdxz","F");
+                break;
+            case 3:
+                f =  PoiMenuTabSharedFragment.newInstance("bvdxz","F");
+                break;
+
+        }
+        return f;
+    }
+
+    //TODO delete this, get real data
+    List<PointOfInterest> testData()
+    {
+        PointOfInterest poi1 = new PointOfInterest("poi1","poi1",new LatLng(51.74869,19.45537));
+        PointOfInterest poi2 = new PointOfInterest("poi2","poi2",new LatLng(51.74893,19.45957));
+        PointOfInterest poi3 = new PointOfInterest("poi3","poi3",new LatLng(51.74548,19.46182));
+        PointOfInterest poi4 = new PointOfInterest("poi4","poi4",new LatLng(51.74086,19.46393));
+        PointOfInterest poi5 = new PointOfInterest("poi4","poi4",new LatLng(51.71092,19.48337));
+
+        ArrayList<PointOfInterest> poiList = new ArrayList<>();
+        poiList.add(poi1);
+        poiList.add(poi2);
+        poiList.add(poi3);
+        poiList.add(poi4);
+        poiList.add(poi5);
+        return poiList;
+    }
+}
+
